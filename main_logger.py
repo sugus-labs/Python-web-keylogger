6ab525a8f5887db3a49ffd111e050d66f645f104
gmartinvela <gustavo.martin@opendeusto.es>
2013-12-29 10:31:55 +0100

    Deleted some files to simplify the code. In these momments all the events are managed by evdev

--- main_logger.py
+++ main_logger.py
@@ -10,28 +10,43 @@ IT IS NECESSARY TO PLAY THIS SCRIPT WITH ROOT PRIVILEGES (SUDO)
 
 # To install evdev library: sudo pip install evdev
 # evdev library webpage: http://pythonhosted.org/evdev/
-from evdev import InputDevice, list_devices
+from evdev import InputDevice, list_devices, categorize, ecodes
+# Necessary to read well multiple devices
+from select import select
 
 # This list is to enter the strings in uppercase that we want to search
-wanted_devices_list = ["USB", "KEYBOARD"]
+wanted_devices_string_list = ["USB", "KEYBOARD"]
 # This dictionnary is to save the devices that maps with our strings
 interesting_devices_dict = {}
 # This mapping maps each device to /dev/input/eventX
 devices = map(InputDevice, list_devices())
 
+# A new list to save only the wanted devices 
+wanted_devices_list = []
 # Searching in all the devices
 for dev in devices:
 	# Converting device names to uppercase to process well
 	name_upper = dev.name.upper()
 	# Iterating over our wanted strings
-	for wanted_string in wanted_devices_list:
+	for wanted_string in wanted_devices_string_list:
 		# Searching our string in the names of the devices
 		if wanted_string in name_upper:
 			# Saving in the dictionnary
 			interesting_devices_dict[wanted_string] = dev
+			# Put all the wanted devives in the new tuple
+			wanted_devices_list.append(dev.fn)
 	#print( '%-20s %-32s %s' % (dev.fn, dev.name, dev.phys) )
 
 # Searching in all the interesting devices
-for key, device in interesting_devices_dict.iteritems():
+#for key, device in interesting_devices_dict.iteritems():
 	# Mapping all the capabilities of the device
-	print "%s\n%s" % (key, device.capabilities(verbose=True))
+	#print "%s\n%s" % (key, device.capabilities(verbose=True))
+
+devices = map(InputDevice, wanted_devices_list)
+devices = {dev.fd : dev for dev in devices}
+
+while True:
+	r,w,x = select(devices, [], [])
+	for fd in r:
+		for event in devices[fd].read():
+			print(event)
